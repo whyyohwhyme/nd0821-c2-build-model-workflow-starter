45249e33cd73db1164e56c68fee0fe24f059ec04
yolan royan <yolan.royan@gmail.com>
2022-01-17 11:51:24 +0200

    Added extra checks + test data coplete

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -59,7 +59,14 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
+def test_row_count(data: pd.DataFrame):
+    """
+    Test that the dataset has a reasonable number of rows
+    """
+    assert 15000 < data.shape[0] < 1000000
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_price_range(data: pd.DataFrame, min_price : float, max_price: float):
+    """
+    Checks if the prices fall within the bounds specified in the config
+    """
+    assert data['price'].between(min_price, max_price).all()
