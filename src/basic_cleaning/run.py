3e4695f542aed774ecddd8b5941d9fbda3622677
yolan royan <yolan.royan@gmail.com>
2022-01-17 12:50:51 +0200

    Added lat/long area restriction to datacleaning

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -29,6 +29,10 @@ def go(args):
 
     idx = df.price.between(args.min_price, args.max_price)
     df = df[idx].copy()
+
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     df.to_csv('clean_sample.csv', index=False)
 
     artifact = wandb.Artifact(
