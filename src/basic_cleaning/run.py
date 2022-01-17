51098a3bbfe6eddbcbd0ca459f074b5a82dc1f00
yolan royan <yolan.royan@gmail.com>
2022-01-17 11:51:00 +0200

    Basic cleaning complete

---
+++ src/basic_cleaning/run.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+"""
+Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
+"""
+import argparse
+import logging
+import wandb
+import pandas as pd
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    run = wandb.init(job_type="basic_cleaning")
+    run.config.update(args)
+
+    # Download input artifact. This will also log that this script is using this
+    # particular version of the artifact
+    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+
+    ######################
+    # YOUR CODE HERE     #
+    ######################
+
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+    df = pd.read_csv(artifact_local_path)
+
+    idx = df.price.between(args.min_price, args.max_price)
+    df = df[idx].copy()
+    df.to_csv('clean_sample.csv', index=False)
+
+    artifact = wandb.Artifact(
+        args.output_artifact, 
+        type=args.output_type, 
+        description=args.output_description,
+    )
+
+    artifact.add_file('clean_sample.csv')
+    run.log_artifact(artifact)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
+
+
+    parser.add_argument(
+        "--input_artifact", 
+        type=str,
+        help="Raw data to be cleaned",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_artifact", 
+        type=str,
+        help="Name of cleaned artifact to store in Weights and biases",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_type", 
+        type=str,
+        help='The type of artifact that gets created, usually a dataset',
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_description", 
+        type=str,
+        help='Description for the output dataset',
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price", 
+        type=float,
+        help='the minimum value for price, any price below this will be removed',
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price", 
+        type=float,
+        help='the maximum value for price, all prices above this will be removed',
+        required=True
+    )
+
+
+    args = parser.parse_args()
+
+    go(args)
